369cdfcb0bf9d22c2527129a7f65c2bd7009ceb1
Ray Villalobos <ray@planetoftheweb.com>
2018-09-10 07:54:31 -0400

    got rid of container on app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 class App extends Component {
   render() {
     return (
-      <div className="container App">
+      <div className="App">
         <h1>Meeting Log</h1>
       </div>
     );
