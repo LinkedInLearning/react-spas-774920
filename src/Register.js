1c069c20dcb352fd81bb1fe40290f9ba09122f1c
Ray Villalobos <ray@planetoftheweb.com>
2018-09-11 18:04:46 -0700

    02_04e

--- src/Register.js
+++ src/Register.js
@@ -1,11 +1,106 @@
 import React, { Component } from 'react';
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      displayName: '',
+      email: '',
+      passOne: '',
+      passTwo: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const itemName = e.target.name;
+    const itemValue = e.target.value;
+
+    this.setState({ [itemName]: itemValue });
+  }
+
   render() {
     return (
-      <div className="text-center mt-4">
-        <h1 className="text-primary">Register</h1>
-      </div>
+      <form className="mt-3">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8">
+              <div className="card bg-light">
+                <div className="card-body">
+                  <h3 className="font-weight-light mb-3">Register</h3>
+                  <div className="form-row">
+                    <section className="col-sm-12 form-group">
+                      <label
+                        className="form-control-label sr-only"
+                        htmlFor="displayName"
+                      >
+                        Display Name
+                      </label>
+                      <input
+                        className="form-control"
+                        type="text"
+                        id="displayName"
+                        placeholder="Display Name"
+                        name="displayName"
+                        required
+                        value={this.state.displayName}
+                        onChange={this.handleChange}
+                      />
+                    </section>
+                  </div>
+                  <section className="form-group">
+                    <label
+                      className="form-control-label sr-only"
+                      htmlFor="email"
+                    >
+                      Email
+                    </label>
+                    <input
+                      className="form-control"
+                      type="email"
+                      id="email"
+                      placeholder="Email Address"
+                      required
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.handleChange}
+                    />
+                  </section>
+                  <div className="form-row">
+                    <section className="col-sm-6 form-group">
+                      <input
+                        className="form-control"
+                        type="password"
+                        name="passOne"
+                        placeholder="Password"
+                        value={this.state.passOne}
+                        onChange={this.handleChange}
+                      />
+                    </section>
+                    <section className="col-sm-6 form-group">
+                      <input
+                        className="form-control"
+                        type="password"
+                        required
+                        name="passTwo"
+                        placeholder="Repeat Password"
+                        value={this.state.passTwo}
+                        onChange={this.handleChange}
+                      />
+                    </section>
+                  </div>
+                  <div className="form-group text-right mb-0">
+                    <button className="btn btn-primary" type="submit">
+                      Register
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
     );
   }
 }
