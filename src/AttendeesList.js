47a0f3da999a166c629e9b6b36264a104c321ca4
Ray Villalobos <ray@planetoftheweb.com>
2018-09-14 18:22:41 -0700

    05_02e

--- src/AttendeesList.js
+++ src/AttendeesList.js
@@ -8,16 +8,18 @@ class AttendeesList extends Component {
   render() {
     const attendees = this.props.attendees;
     const myAttendees = attendees.map(item => {
-      <div
-        className="col-8 col-sm-6 col-md-4 col-lg-3 mb-2 p-0 px-1"
-        key={item.attendeeID}
-      >
-        <div className="card ">
-          <div className="card-body px-3 py-2 d-flex align-items-center justify-content-center">
-            <div>{item.attendeeName}</div>
+      return (
+        <div
+          className="col-8 col-sm-6 col-md-4 col-lg-3 mb-2 p-0 px-1"
+          key={item.attendeeID}
+        >
+          <div className="card ">
+            <div className="card-body px-3 py-2 d-flex align-items-center justify-content-center">
+              <div>{item.attendeeName}</div>
+            </div>
           </div>
         </div>
-      </div>;
+      );
     });
 
     return (
