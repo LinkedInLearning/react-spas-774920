0922523f51ae301a2e0a4133af1927fe925bddd1
Ray Villalobos <ray@planetoftheweb.com>
2018-09-16 14:30:58 -0700

    05_06e

--- src/AttendeesList.js
+++ src/AttendeesList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { GoTrashcan, GoStar } from 'react-icons/go';
+import { GoTrashcan, GoMail, GoStar } from 'react-icons/go';
 import firebase from './Firebase';
 
 class AttendeesList extends Component {
@@ -74,6 +74,13 @@ class AttendeesList extends Component {
                   >
                     <GoStar />
                   </button>
+                  <a
+                    href={`mailto:${item.attendeeEmail}`}
+                    className="btn btn-sm btn-outline-secondary"
+                    title="Mail Attendee"
+                  >
+                    <GoMail />
+                  </a>
                   <button
                     className="btn btn-sm btn-outline-secondary"
                     tite="Delete Attendee"
